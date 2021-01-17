c4fa98592e266fd4d13c55f2767be924da758027
BichengWang <bichengwang17@gmail.com>
2021-01-17 10:55:43 -0800

    Investment Principle

--- src/finance/investment_strategy/investment_2021.py
+++ src/finance/investment_strategy/investment_2021.py
@@ -1,4 +1,4 @@
-key_words = ["Trending", "Fact", "Reason", "Side Effect", "Index", "Stock"]
+key_words = ["Trending", "Fact", "Reason", "Side Effect", "Index", "Stock", "ETF"]
 knowledge_graph_dict = {
     "Value Investing": {},
     "Risk Management": {},
@@ -20,13 +20,30 @@ knowledge_graph_dict = {
             "Fact": "Negative",
             "Side Effect": {
                 "Consumption Discretionary Sector": {
-                    "Index": "",
+                    "ETF": [
+                        "PEJ Leisure & entertainment etf",
+                        "XLY Consumer discretionary select sector SPDR fund",
+                    ],
                     "Trending": "Positive",
                     "Reason": "Covid recover and stimulus package delivered."
                 }
             }
         },
     }
+}
+
+
+investment_principle = {
+    "principle1": {
+        "Principle": "Other investors haven't go into the underlying but will plan to.",
+        "Trending": "Positive",
+        "Reason": "Newly long position crowd would push the price to go high and squeeze short."
+    },
+    "principle2": {
+        "Principle": "Does all the potential investors go into the trading? Yes, then leave it",
+        "Trending": "Negative",
+        "Reason": "Picked like leek",
+    },
 }
 
 
