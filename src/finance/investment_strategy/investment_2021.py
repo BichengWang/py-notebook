01099af502fc3dc7f03088b85d9a26f0ff6bb91b
BichengWang <bichengwang17@gmail.com>
2021-01-17 10:18:48 -0800

    Investment 2021 Plan

---
+++ src/finance/investment_strategy/investment_2021.py
@@ -0,0 +1,38 @@
+key_words = ["Trending", "Fact", "Reason", "Side Effect"]
+knowledge_graph_dict = {
+    "Value Investing": {},
+    "Risk Management": {},
+    "Joe Biden": {
+        "Tax Increase Policy": {
+            "Tech Company": {
+                "Trending": "Negative",
+            },
+            "Corporate Tax": {
+                "Trending": "Negative",
+            },
+            "Capital Gain Tax": {
+                "Trending": "Negative",
+            }
+        },
+        "Economic Stimulus Package": {
+            "Trending": "Positive",
+            "Reason": "buy the rumor sell the news.",
+            "Fact": "Negative",
+            "Side Effect": {
+                "Consumption Discretionary Industry": {
+                    "Trending": "Positive",
+                    "Reason": "Covid recover and stimulus package delivered."
+                }
+            }
+        },
+    }
+}
+
+
+class Investment2021:
+    def __init__(self):
+        self.knowledge_graph_dict = knowledge_graph_dict
+
+
+if __name__ == "__main__":
+    print("Investment Jan. Plan")
