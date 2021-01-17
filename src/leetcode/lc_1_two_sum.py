98473abc98c126fc127f9d4422a4513c345c967a
BichengWang <bichengwang17@gmail.com>
2021-01-17 10:02:17 -0800

    Investment Plan

---
+++ src/leetcode/lc_1_two_sum.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class TwoSum:
+    def two_sum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = dict()
+        for i, num in enumerate(nums):
+            nums_dict[num] = i
+            if target - num in nums_dict:
+                return [nums_dict[target - num], i]
+        return [-1, -1]
+
+
+if __name__ == "__main__":
+    print(TwoSum().two_sum(nums=[2,7,11,15], target=9))
