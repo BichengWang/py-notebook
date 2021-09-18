e9ab0e7a87449e103d2c780a2beac20551f1c15b
Bicheng Wang <bicheng.wang@uber.com>
2021-09-17 18:26:20 -0700

    ci

--- src/leetcode/interviews/robinhood_interviews_test.py
+++ src/leetcode/interviews/robinhood_interviews_test.py
@@ -1,13 +1,13 @@
 from unittest import TestCase
 
-from src.leetcode import interview_collections
+from src.leetcode.interviews import robinhood_interviews
 
 
 class Test(TestCase):
     def test_exact_match(self):
         self.assertEqual(
             (['FB,B,0100,UUID1'], ['FB,S,0100,UUID1']),
-            interview_collections.delete1([
+            robinhood_interviews.delete1([
                 "AAPL,B,0100,UUID1",
                 "FB,B,0100,UUID1",
             ], [
@@ -19,7 +19,7 @@ class Test(TestCase):
     def test_match_fuzzy(self):
         self.assertEqual(
             (['FB,B,0100,UUID1'], ['FB,S,0100,UUID1']),
-            interview_collections.delete2([
+            robinhood_interviews.delete2([
                 "AAPL,B,0100,UUID1",
                 "FB,B,0100,UUID1",
             ], [
@@ -31,7 +31,7 @@ class Test(TestCase):
     def test_match_buy_sell(self):
         self.assertEqual(
             (['AAPL,S,0100,UUID1'], ['AAPL,B,0100,UUID2']),
-            interview_collections.delete3([
+            robinhood_interviews.delete3([
                 "AAPL,B,0100,UUID1",
                 "FB,B,0100,UUID1",
             ], [
