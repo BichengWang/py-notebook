6c95542e1a03934575035b61614a060e91e935ca
Bicheng Wang <bichengwang17@gmail.com>
2024-10-02 23:24:12 -0700

    update portfolio

--- src/finance/2024_investment/portfolio_calc.py
+++ src/finance/2024_investment/portfolio_calc.py
@@ -1,9 +1,10 @@
 from datetime import date, timedelta, datetime
 import pandas as pd
 import numpy as np
+import sys
 
 
-def pnl():
+def pnl(robinhood, ib, fidelity, moomoo):
     # spy return rate
     spy_rate = 568.44/467.58
     today = datetime.today()
@@ -16,18 +17,26 @@ def pnl():
     ))
     transfer_base = np.sum(df['price'] * df['number'])
     begin = 2294.66
-    current = 345344.47
+    current = moomoo
+    
     pnl = current - begin - transfer_base
     spy_expect_return = begin * (daily_rate ** delta) + np.sum(df['spy_expect_return'])
     print(f"pnl: {pnl:.2f}\npnl_rate:{pnl/(begin+transfer_base):.2f}\ncurrent: {current:.2f}\nspy expect: {spy_expect_return:.2f}")
-    
-    robinhood, ib, fidelity = 631147.47, 28066.44, 100527.97
     total_assets = current + robinhood + ib + fidelity
     print(f"moomoo: {current:.2f}\nrobinhood: {robinhood:.2f}\nib: {ib:.2f}\nfidelity: {fidelity:.2f}\ntotal assets: {total_assets:.2f}")
 
 
 if __name__ == '__main__':
     """
-    portfolio 2
+    portfolio
+    
+    robinhood, ib, fidelity = 626720.13 30844. 100024.19 356489.09
     """
-    pnl()
+    if len(sys.argv) != 5:
+        print("Usage: python portfolio_calc.py <robinhood> <ib> <fidelity>")
+        sys.exit(1)
+    robinhood = float(sys.argv[1])
+    ib = float(sys.argv[2])
+    fidelity = float(sys.argv[3])
+    moomoo = float(sys.argv[4])
+    pnl(robinhood, ib, fidelity, moomoo)
